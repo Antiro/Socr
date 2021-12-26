173b8fe34d81645d2328a1251bedb5dc17914d68
Antiro <81457725+Antiro@users.noreply.github.com>
2021-12-26 16:51:53 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from os import name
+from re import I
 from flask import Flask, request, render_template, redirect, url_for
 import pyshorteners
 import sqlite3
 import hashlib
 import time
-
 
 # БД
 conn = sqlite3.connect('db.db', check_same_thread=False)
@@ -34,7 +35,7 @@ def logi(log,pas):
     heLog = hashlib.md5(log.encode()).hexdigest()
     hePas = hashlib.md5(pas.encode()).hexdigest()
 
-    if cursor.execute(f"SELECT * FROM users WHERE login = '{heLog}'").fetchone() is None:
+    if cursor.execute(f"SELECT id FROM users WHERE login = '{heLog}'").fetchone() is None:
         return 0
         # нет такого пользователя
     else:
@@ -52,11 +53,12 @@ def logi(log,pas):
 
 # выход из аккаунта
 def exit(id):
-    # time.sleep(5)
-    cursor.execute(f"DELETE FROM users_online WHERE id={id}").fetchone()
+    cursor.execute(f"DELETE FROM users_online WHERE id={id}")
     conn.commit()
-    print("Вы из вышли аккаунта")
 
+
+def user(id):
+    return cursor.execute(f"SELECT * FROM users WHERE id = '{id}'").fetchall()
 
 # 
 # 
@@ -87,15 +89,14 @@ def add_genURL(id,url):
 def allGebUrlUser(id_user):
     arr=[]
     if cursor.execute(f"SELECT * FROM users_online WHERE id = {id}").fetchone() is None:
-       arr.append('Вы не зарегестрированы, войдите в аккаунт')
+        print('Вы не зарегестрированы, войдите в аккаунт')
     else:
         url=cursor.execute(f"SELECT * FROM general WHERE id_user = {id_user}").fetchall()
-        
         if url == []:
-            arr.append("У вас нет публичных ссылок")
+            print("У вас нет публичных ссылок")
         else:
             for i in range(len(url)):
-                arr.append(f'id - {url[i][0]} / URL - <a href="{url[i][2]}">{url[i][2]}</a>')
+                arr.append([url[i][0],url[i][2]])
                 
     return arr
 
@@ -154,17 +155,16 @@ def add_prURL(id_user,url):
 
 # все приватные ссылки пользователя
 def allPrUrlUser(id_user):
-    if cursor.execute(f"SELECT * FROM users_online WHERE id = {id}").fetchone() is None:
-        print('Вы не зарегестрированы, войдите в аккаунт')
+    arr=[]
+    url=cursor.execute(f"SELECT * FROM private WHERE id_user = {id_user}").fetchall()
+    if url == []:
+        arr.append(None)
     else:
-        url=cursor.execute(f"SELECT * FROM private WHERE id_user = {id_user}").fetchall()
-        if url == []:
-            print("У вас нет приватных ссылок")
-        else:
-            print("Ваши приватные сокращенные ссылки:")
-            for i in range(len(url)):
-                print(f'id - {url[i][0]} / URL - {url[i][2]}')
-
+        for i in range(len(url)):
+            arr.append([url[i][0],url[i][2]])
+    
+    return arr
+    
 
 # удаление приватной ссылки пользователя
 def delPrUrl(id_user,id):
@@ -184,11 +184,11 @@ def delPrUrl(id_user,id):
 app = Flask(__name__)
 
 
-# вход
 @app.route('/')
 def index():
     return render_template('index.html',title="Вход")
-
+    
+# вход
 @app.route('/sub',methods = ['POST', 'GET'])
 def login():
     name = request.form['Name']
@@ -197,6 +197,7 @@ def login():
     log=logi(name,password)
       
     if log == 1:
+
         return redirect(url_for('pub'))
     elif log == 0:
         info="Нет такого ползователя"
@@ -225,19 +226,58 @@ def registr():
         return redirect(url_for('index'))
 
 
+# выход
+@app.route('/exit')
+def exitUser():
+    global id
+    exit(id)
+    id = 0
+    return redirect(url_for('index'))
+
+# добавление публичной ссылки
+@app.route('/addPub',methods = ['POST', 'GET'])
+def addPubURL():
+    url = request.form['url']
+    global id
+    add_genURL(id,url)
+    return redirect(url_for('pub'))
 
 # публичные ссылки
 @app.route('/pub')
 def pub():
-    urlP=allGebUrl()
-    return render_template('pub.html',urlAll=urlP)
+    global id 
+    if id == 0:
+        return redirect(url_for('index'))
+    else:
+        urlP=allGebUrl()
+        pubURLuser=allGebUrlUser(id)
+        return render_template('pub.html',urlAll=urlP,URLuser=pubURLuser)
 
 
+# приватные ссылки
 @app.route('/private')
 def private():
-    return render_template('private.html',title="Приватные ссылки")
+    global id 
+    if id == 0:
+        return redirect(url_for('index'))
+    else:
+        temp=allPrUrlUser(id)
+        # if temp == None:
+        #     prURL=['none']
+        # else:
+        prURL= temp
+        return render_template('private.html',title="Приватные ссылки",prURL=prURL)
 
 
 
+# добавление приватной ссылки
+@app.route('/addPr',methods = ['POST', 'GET'])
+def addPrURL():
+    url = request.form['url']
+    global id
+    add_prURL(id,url)
+    return redirect(url_for('private'))
+
+    
 if __name__ == '__main__':
     app.run()
